8217b018fed93049dcc7283d3bdde93673e49049
mahfuj80 <mahfujurrahman06627@gmail.com>
2023-09-01 17:03:11 +0600

    if no video is availale on any catagory the show error messgae added

--- scripts/index.js
+++ scripts/index.js
@@ -20,11 +20,34 @@ const createCategoryItem = (categoriesName) => {
     document.getElementById('1000').click();
 }
 
-async function categoryDisplay(key) {
+let rememberKey = '';
+async function categoryDisplay(key, isShort = 0) {
+    // Need Previous key for Shorting by view;
+    if (key === 'previousKey') {
+        key = rememberKey;
+    };
     const id = key.toString();
+    rememberKey = id;
     const singleCategoryResponse = await fetch(`https://openapi.programming-hero.com/api/videos/category/${id}`);
     const singleCategory = await singleCategoryResponse.json();
     const isDataAvailable = singleCategory.status;
+    const itemsContainer = document.getElementById('items-container')
+    if (!isDataAvailable) {
+        itemsContainer.innerHTML = '';
+        itemsContainer.innerHTML = `
+        <div class="flex flex-col justify-center items-center gap-8 py-10 h-[70vh] m-auto">
+            <img class="" src="./images/Icon.png" alt="Icon">
+            <h1 class="text-black font-bold text-4xl text-center">Oops!! Sorry, There is no <br> content here</h1>
+        </div>
+        `
+    } else if (isDataAvailable) {
+        itemsContainer.innerHTML = '';
+        const singleCategoryItems = singleCategory.data;
+        console.log(singleCategoryItems);
+        singleCategoryItems.forEach(singleItems => {
+            console.log(singleItems.category_id);
+        });
+    }
     // console.log(typeof isDataAvailable);
 }
 
